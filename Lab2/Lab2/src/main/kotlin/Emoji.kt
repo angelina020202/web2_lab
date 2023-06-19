5f616ee39bacd4b87f44908d3ff2ec333c504ddb
angelina020202 <74893844+angelina020202@users.noreply.github.com>
2023-06-19 17:05:10 +0700

    Delete Lab2/Lab2 directory

--- Lab2/Lab2/src/main/kotlin/Emoji.kt
+++
@@ -1,9 +0,0 @@
-package com.example.json
-
-data class Emoji(
-    val name: String,
-    val category: String,
-    val group: String,
-    val htmlCode: List<String>,
-    val unicode: List<String>
-)
